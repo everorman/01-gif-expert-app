5b525e1a3ec8283db9f317d1a9270e892e2b6af2
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-04-23 17:08:27 -0500

    chore: adding event to parent

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -10,7 +10,7 @@ export const AddCategory = ({ setCategories }) => {
   const onSubmit = (event) => {
     event.preventDefault();
     if (inputValue.trim().length <= 1) return;
-    setCategories(inputValue);
+    setCategories(inputValue.trim());
     setInputValue('');
   };
 
