5afd953c122c2f8111df47c729f44977a61e65ea
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-04-30 11:40:16 -0500

    chore: adding getGif test

---
+++ test/helpers/getGift.test.js
@@ -0,0 +1,12 @@
+import { getGifs } from '../../src/helpers/getGifts';
+describe('GetGifService', () => {
+  it('should return a gif array', async () => {
+    const gifs = await getGifs('One punch');
+    expect(gifs.length).toBeGreaterThan(0);
+    expect(gifs[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      url: expect.any(String)
+    });
+  });
+});
